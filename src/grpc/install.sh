1863e70c2b0f0acf472f6f88c5add685f47b5a14
liuxingjun <connliu@qq.com>
2023-05-12 03:32:22 +0000

    fix curl zip

--- src/grpc/install.sh
+++ src/grpc/install.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+check_packages pkg-config curl ca-certificates unzip
 # proto
 export PROTOC_VERSION="${PROTOC_VERSION:-"3.20.1"}"
 export PB_REL="https://github.com/protocolbuffers/protobuf/releases"
